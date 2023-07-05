27fa45a2f8a488ff596177280fad6814d670e68b
bieu-bonfim <gabrielmmbonfim@gmail.com>
2023-07-05 12:25:08 -0300

    final final final

--- PILHA/run.py
+++ PILHA/run.py
@@ -1,9 +1,16 @@
 from PILHA.Mina import *
 from PILHA.Anao import *
 from PILHA.Crafting import *
+from Auxiliar import roll
+
+def genStr(size=2):
+    mina_str = ''
+    for i in range(size):
+        mina_str = mina_str + str(roll(10)-1)
+    return mina_str
 
 def run_pilha():
-    mina = Mina('Moria', '9')
+    mina = Mina('Moria', genStr(roll(5)))
     mina.criaMina()
     anao = Anao('Ballin', mina)
     anao.explorar()
