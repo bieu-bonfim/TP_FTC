f31363ae468f991e2f75a3157fe0d95504d94782
bieu-bonfim <gabrielmmbonfim@gmail.com>
2023-07-05 12:30:22 -0300

    FINAL

--- PILHA/run.py
+++ PILHA/run.py
@@ -4,10 +4,15 @@ from PILHA.Crafting import *
 from Auxiliar import roll
 
 def genStr(size=2):
-    mina_str = ''
-    for i in range(size):
-        mina_str = mina_str + str(roll(10)-1)
-    return mina_str
+    opt = int(input('Escolha o modo:\n1- Caverna aleatória\n2- Caverna pré-definida'))
+    if opt == 1:
+        mina_str = ''
+        for i in range(size):
+            mina_str = mina_str + str(roll(10)-1)
+        return mina_str
+    elif opt == 2:
+        mapa = input('\nInsira a caverna: ')
+        return mapa
 
 def run_pilha():
     mina = Mina('Moria', genStr(roll(5)))
