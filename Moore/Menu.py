701414d1c9794dc24327508892d981276bfa1da7
Guilherme-Schwann <guilhermeawilke@hotmail.com>
2023-07-05 11:52:39 -0300

    moore menu

--- Moore/Menu.py
+++ Moore/Menu.py
@@ -13,19 +13,21 @@ dos seus guerreiros para lutar pela liberdade e pela honra. \x1b[0m'.center(175)
 detalhe as características de seu guerreiro logo abaixo. \x1b[0m'.center(185), "\n")
   print_slow(f'\x1b[1m Que o destino seja definido na arena de \x1b[31mVermécia\x1b[0m!\n'.center(180), "\n\n")
   
-  print(f'\x1b[1mInsira o \x1b[34mnome de seu pergaminho\x1b[0m')
+  print_slow(f'\x1b[1mInsira o \x1b[34mnome de seu pergaminho\x1b[0m', "\n")
   file = input(' >> ')
-  print(f'\x1b[1mQual o \x1b[34mnome de seu campeão\x1b[39m?\x1b[0m')
+  print_slow(f'\x1b[1mQual o \x1b[34mnome de seu campeão\x1b[39m?\x1b[0m', "\n")
   guerr = input(' >> ')
-  print(f'\x1b[1mQual o \x1b[34mnome de seu reinado\x1b[39m?\x1b[0m')
+  print_slow(f'\x1b[1mQual o \x1b[34mnome de seu reinado\x1b[39m?\x1b[0m', "\n")
   reino = input(' >> ')
   
-  print(f'\x1b[1mAgora, insira \
-a {Fore.MAGENTA}distribuição de pontos de status\x1b[39m de seu guerreiro: \x1b[0m')
+  print_slow(f'\x1b[1mAgora, insira \
+a {Fore.MAGENTA}distribuição de pontos de status\x1b[39m de seu guerreiro: \x1b[0m', "\n")
         
-  print(f'\x1b[1mOBS: Você possui 10 pontos, insira a pontuação com separação de')
-  print(f'\x1b[1mespaços na ordem DEX STR WIS:')
+  print_slow(f'\x1b[1mOBS: Você possui 10 pontos, insira a pontuação \
+com separação de espaços na ordem DEX STR WIS:\x1b[0m', "\n")
   points = input('>> ')
+  print()
+  os.system("clear")
   points = points.strip().split()
   maq = Guerreiro(file, reino, guerr, int(points[0]), int(points[1]), int(points[2]))
   
