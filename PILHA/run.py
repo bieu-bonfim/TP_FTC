c303737cac8e405009e844987c90e90cfded83bd
patrickeraa <patrick.araujo@ufv.br>
2023-07-05 11:29:26 -0300

    alteração do run.py

--- PILHA/run.py
+++ PILHA/run.py
@@ -2,7 +2,7 @@ from Mina import *
 from Anao import *
 from Crafting import *
 
-mina = Mina('Moria', '9')
+mina = Mina('Moria', '10201101')
 mina.criaMina()
 anao = Anao('Ballin', mina)
 anao.explorar()
